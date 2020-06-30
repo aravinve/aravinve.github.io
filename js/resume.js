c34d054c5bd73c2651611839db3151ec51eaf18b
Aravind Venkatesan <aravindtherocker@gmail.com>
2020-06-30 12:03:08 +0800

    academic update

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,6 @@
 (function ($) {
   'use strict'; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, '') ==
@@ -23,15 +22,74 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#sideNav',
   });
+
+  $('#academic-projects-link').click(function () {
+    $('#academic-projects').removeClass("d-none");
+    $('#academic-projects-link').addClass("active");
+    $('#personal-projects').addClass("d-none");
+    $('#personal-projects-link').removeClass("active");
+  })
+
+  $('#personal-projects-link').click(function () {
+    $('#personal-projects').removeClass("d-none");
+    $('#personal-projects-link').addClass("active");
+    $('#academic-projects').addClass("d-none");
+    $('#academic-projects-link').removeClass("active");
+  })
+
+  var form = document.getElementById("contact-form");
+  var alert = document.getElementById('alert-div');
+  function success(){
+    form.reset();
+    alert.classList.add("alert-success")
+    alert.classList.remove("d-none")
+    alert.innerText = 'Email Sent Successfully!'
+    setTimeout(() => {
+      alert.classList.remove("alert-success")
+      alert.classList.add("d-none")
+      alert.innerText = ''
+    }, 3000)
+  }
+
+  function error(){
+    alert.classList.add("alert-danger")
+    alert.classList.remove("d-none")
+    alert.innerText = 'Failed! Try Again Later'
+    setTimeout(() => {
+      alert.classList.remove("alert-danger")
+      alert.classList.add("d-none")
+      alert.innerText = ''
+    }, 3000)
+  }
+
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+
+  function ajax(method, url, data, success, error){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
 
   $('#modal-close-btn').click(function () {
     $('#projectModalLabel').text('');
@@ -44,6 +102,34 @@
     $('.modal-body .content').text('');
     $('#github_uri').html('Link');
     $('#github_uri').attr('href', '#!');
+  });
+
+  $('#sparkfab').click(function () {
+    $('#projectModalLabel').html(
+      'Sparkfab'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/sparkfab/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
+      'Developed a multipurpose platform for fashion image classification and search. Techstack involved Pyspark, SparkDL, Django and Elasticsearch'
+    );
+    $('#github_uri').html('<i class="fab fa-github"> </i> Github');
+    $('#github_uri').attr('href', 'https://github.com/aravinve/CS4225-CS5425-BigDataProject');
+  });
+
+  $('#blockbustr').click(function () {
+    $('#projectModalLabel').html(
+      'Blockbustr'
+    );
+    $('img[class="d-block w-100"]').each(function (index, element) {
+      $(element).attr('src', `./img/gif/blockbustr/${index + 1}.png`);
+    });
+    $('.modal-body .content').text(
+      'MERN Stack web app demonstrating Cross-Site Scripting and HTTP Parameter Pollution problems and the relevant defenses in form of database scanning, data filtration, and deployment of attack detection middleware.'
+    );
+    $('#github_uri').html('<i class="fab fa-github"> </i> Github');
+    $('#github_uri').attr('href', 'https://github.com/aravinve/blockbustr');
   });
 
   $('#rv-labs').click(function () {
